3723eccfb0c61eb22e47b763e44fa1f0a648de79
Laurena Fayad <laurenafayad@gmail.com>
2022-01-27 13:57:23 +0200

    Added functions and event listeners to buttons

--- js-solution.js
+++ js-solution.js
@@ -4,13 +4,21 @@ var audio_green = new Audio("/sounds/green.mp3")
 var audio_red = new Audio("/sounds/red.mp3")
 var audio_yellow = new Audio("/sounds/yellow.mp3")
 var audio_blue = new Audio("/sounds/blue.mp3")
+var audio_wrong = new Audio("/sounds/wrong.mp3")
+
 
 var green_btn = document.getElementById("green")
 var red_btn = document.getElementById("red")
 var yellow_btn = document.getElementById("yellow")
 var blue_btn = document.getElementById("blue")
+var title = document.getElementById("title")
+var game_over_text = "Game Over, Press Any Key To Restart"
+var game_over = false
+var user_turn = false
 
-level_nb = 1
+var level_nb = 1
+var solution = []
+var user_input = []
 
 function pressGreenBtn (){
     audio_green.play()
@@ -37,7 +45,7 @@ function pressYellowBtn (){
 }
 
 function pressBlueBtn (){
-    audio_red.play()
+    audio_blue.play()
     blue_btn.classList.add("pressed");
     setTimeout(function(){
         blue_btn.classList.remove("pressed");
@@ -52,40 +60,49 @@ function randomBtn(){
         pressRedBtn()
     }else if(random_btn == 3){
         pressYellowBtn()
-    }else if (random_btn){
+    }else if (random_btn == 4){
         pressBlueBtn()
     }
+    return random_btn.toString()
 }
 
-green_btn.addEventListener("click", function(){ 
-    pressGreenBtn()
-})
+function getUserInput(){
+    
+    green_btn.addEventListener("click", function(){ 
+        pressGreenBtn()
+        user_input.push("1")
+    })
+    
+    red_btn.addEventListener("click", function(){
+        pressRedBtn()
+        user_input.push("2")
+    })
+    
+    yellow_btn.addEventListener("click", function(){
+        pressYellowBtn()
+        user_input.push("3")
+    })
+    
+    blue_btn.addEventListener("click", function(){
+        pressBlueBtn()
+        user_input.push("4")
+    })
+}
 
-red_btn.addEventListener("click", function(){
-    pressRedBtn()
-})
 
-yellow_btn.addEventListener("click", function(){
-    pressYellowBtn()
-})
-
-blue_btn.addEventListener("click", function(){
-    pressBlueBtn()
-})
-
-
+function gameOver(){
+    document.body.classList.add("game-over");
+    audio_wrong.play()
+    title.textContent = game_over_text
+    document.body.style.backgroundColor = "red"
+    setTimeout(function(){
+        document.body.style.backgroundColor = "#0a2351"
+    }, 300); 
+}
 
 document.addEventListener("keypress", function(event) {
-    document.getElementById("title").textContent = "Level " + level_nb
-    randomBtn()
-
-
-
-
-
-
-
-
-
-
-}); // end of game
+  
+    title.textContent = "Level " + level_nb
+    solution.push(randomBtn())
+    
+}) // end of game
